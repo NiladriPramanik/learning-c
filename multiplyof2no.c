bee1f4e4028ab9c522cb96f10161feac9cafdb30
Niladri Pramanik <niladrip21btechcse2025@kccitm.edu.in>
2022-07-08 21:28:46 +0530

    multiply of 2 numbers

--- multiplyof2no.c
+++ multiplyof2no.c
@@ -4,6 +4,6 @@ int main()
 int x,y ;
 printf("Enter two numbers for addition:");
 scanf("%d %d",&x,&y);
-printf("Sum of %d and %d is %d",x,y,x+y);
+printf("Sum of %d and %d is %d",x,y,x*y);
 return 0 ;
 }
