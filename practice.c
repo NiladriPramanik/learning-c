4ebdfd6eed19cb1741abc96e791a13832af5baa4
Niladri Pramanik <niladrip21btechcse2025@kccitm.edu.in>
2022-07-17 09:32:34 +0530

    number is posituve or negative

---
+++ practice.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+int x;
+printf("Enter a number :") ;
+scanf("%d",&x) ;
+if(x>0) 
+{
+    printf("THE NUMBER IS POSITIVE");
+}
+if(x<=0)
+{
+    printf("THE NUMBER IS NEGATIVE");
+ } 
+ return 0 ;
+}
