f7ef315a53f5a1294878d6f5d64773d16a787ad6
Niladri Pramanik <niladrip21btechcse2025@kccitm.edu.in>
2022-09-27 17:31:54 +0530

    area of circle

---
+++ areaofcircle.c
@@ -0,0 +1,12 @@
+//code for area of a circle
+#include<stdio.h>
+int main()
+{
+    float radius ;
+    printf("enter the radius of the circle");
+    scanf("%f",&radius);
+    printf("area of the circle is :%f" , 3.14*radius*radius);
+    return 0;
+
+}
+
