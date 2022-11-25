c73ba4f0566f7b87f15ada1f68de21e468e6483c
Niladri Pramanik <niladrip21btechcse2025@kccitm.edu.in>
2022-11-25 11:38:34 +0530

    square using power

---
+++ squareusingpow.c
@@ -0,0 +1,11 @@
+//square of number using math.h
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    float n ;
+    printf("enter number for square:");
+    scanf("%f",&n);
+    printf("%f",pow(n,2));
+    return 0;
+}
