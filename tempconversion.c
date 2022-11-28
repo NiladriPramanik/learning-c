65abfbdd0f7112fb8207a4aad8201f040b06769a
Niladri Pramanik <niladrip21btechcse2025@kccitm.edu.in>
2022-11-28 16:22:58 +0530

    temperature conversion with function

--- tempconversion.c
+++ tempconversion.c
@@ -1,3 +1,4 @@
+// code without function
 #include <stdio.h>
 int main()
 {
@@ -8,3 +9,18 @@ fahrenheit = (celsius * 9 / 5) + 32;
 printf("%.2f Celsius = %.2f Fahrenheit", celsius, fahrenheit);
 return 0;
 }
+//code with function below
+// #include<stdio.h>
+// float converttemp(float celsius);
+
+// int main ()
+
+// {
+//     float far = converttemp(100);
+//     printf("far:%f",far);
+//     return 0;
+// }
+// float converttemp(float celsius){
+//     float far = celsius *9.0/5.0 + 32;
+//     return far ;
+// }
