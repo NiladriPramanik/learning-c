67ff71a279369b9409cda3e8a1ed83737e1dff9c
NiladriPramanik <107825176+NiladriPramanik@users.noreply.github.com>
2023-02-21 22:32:35 +0530

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -5,7 +5,8 @@ int main()
  int i,fact=1,number;    
  printf("Enter a number: ");    
   scanf("%d",&number);    
-    for(i=1;i<=number;i++){    
+    for(i=1;i<=number;i++)
+    {
       fact=fact*i;    
   }    
   printf("Factorial of %d is: %d",number,fact); 
