851b91cd2c4b4ef24d52fea03e7b632d93520520
NiladriPramanik <107825176+NiladriPramanik@users.noreply.github.com>
2023-03-05 12:30:23 +0530

    Update age.c

--- age.c
+++ age.c
@@ -3,7 +3,7 @@
 int main()
 {
     int age;
-    printf("Enter the age:");
+    printf("Enter age:");
     scanf("%d",&age);
     age>=18 && age>60 ? printf("an adult\n") : printf("not an adult\n");
 
